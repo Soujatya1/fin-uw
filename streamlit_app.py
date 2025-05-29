52268cdd880a51a34de69dee8ae71abfb2274f81
Soujatya Das <soujatya548@gmail.com>
2025-05-29 13:26:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -951,7 +951,7 @@ customer_docs_vector_store = InMemoryVectorStore(embeddings)
 
 model = ChatGroq(
     groq_api_key="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", 
-    model_name="llama-3.3-70b-versatile", 
+    model_name="compound-beta", 
     temperature=0.3
 )
 
